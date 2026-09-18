0370b6310a7aedde4bc6fae2385e8f8a9df78739
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-09-18 14:46:15 -0400

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
 import express from 'express'
-import authRoutes from './routes/auth.routes.js'
 import dotnev from 'dotenv'
+import { v2 as cloudinary } from 'cloudinary'
 import cookieParser from 'cookie-parser'
+import authRoutes from './routes/auth.routes.js'
+import userRoutes from './routes/user.routes.js'
+import postRoutes from './routes/post.routes.js'
 import dns from "node:dns/promises"
 import connectMongoDB from './db/connectMongoDB.js'
 dns.setServers(["1.1.1.1"])
 dotnev.config()
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
 const app = express()
 const PORT = process.env.PORT ||5000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/posts', postRoutes)
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
     connectMongoDB()
